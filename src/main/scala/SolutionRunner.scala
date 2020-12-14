6cd598d2bf4e019b0d3187f08cde6755f91112d1
marin123 <Token: 5816f4c73c794739c34e3d7bd30e6998c32009b4>
2020-12-14 21:36:51 +0100

    Added day 14 solution

--- src/main/scala/SolutionRunner.scala
+++ src/main/scala/SolutionRunner.scala
@@ -1,7 +1,7 @@
 import solutions._
 object SolutionRunner {
   def main(args: Array[String]): Unit = {
-    val solution = new Solution13("input13.txt")
+    val solution = new Solution14("input14.txt")
     println(s"Solution is ${solution.getSolution()}")
   }
 }
---
+++ src/main/scala/solutions/Solution14.scala
@@ -0,0 +1,85 @@
+package solutions
+
+import javax.swing.text.AttributeSet.ParagraphAttribute
+
+import scala.io.Source
+
+class Solution14(str: String) extends Solution {
+  val input = Source.fromResource(s"data/$str").getLines.toArray
+  def resolveMask(c: Char, c1: Char)={
+    if (c1 == 'X') c
+    else c1
+  }
+  def calculateValue(currentMask: String, number: Long): Long = {
+    val numberBin = number.toBinaryString.reverse.padTo(36, '0').reverse
+    val newValue = numberBin.zip(currentMask).map(x => resolveMask(x._1, x._2)).mkString("")
+    java.lang.Long.parseLong(newValue.toString, 2)
+  }
+
+  def firstPart(): Long = {
+    val memoryMap: collection.mutable.Map[Int, Long] = collection.mutable.Map(0 -> 0)
+    val reg = "[0-9]+".r
+    var currentMask: String = ""
+    val data = input.map(_.split(" = "))
+    for (operation <- data){
+      if (operation(0) == "mask") currentMask = operation(1)
+      else {
+        val mem: Int = reg.findFirstIn(operation(0)).get.toInt
+        memoryMap(mem) = calculateValue(currentMask, operation(1).toLong)
+        }
+
+      }
+    memoryMap.values.sum
+    }
+
+  def resolveFloatMask(x: (Char, Char)): Char = {
+    if (x._1 == '0') x._2
+    else if (x._1 == '1') '1'
+    else 'X'
+  }
+
+  def substituteFloats(x: String, floatMask: String): String = {
+    var index = 0
+    var outputString = floatMask.toArray
+    for (char <- floatMask.indices){
+      if (floatMask(char) == 'X') {
+        outputString(char) = x(index)
+        index += 1
+      }
+    }
+    outputString.mkString("")
+  }
+
+  def generateAddresses(floatMask: String): Array[Long] = {
+    val amountOfFloats: Int = scala.math.pow(2,floatMask.count(_ == 'X')).toInt
+    val range = 0 until amountOfFloats
+    val subValues = range.map(_.toBinaryString.reverse.padTo(amountOfFloats.toBinaryString.length - 1, '0').reverse).toArray
+    subValues.map(x => substituteFloats(x, floatMask)).map(x => java.lang.Long.parseLong(x.toString, 2))
+  }
+
+  def calculateAddresses(currentMask: String, address: Long): Array[Long] = {
+    val floatMask = currentMask.zip(address.toBinaryString.reverse.padTo(36, '0').reverse)
+      .map(x => resolveFloatMask(x)).toArray
+    generateAddresses(floatMask.mkString(""))
+  }
+
+  def secondPart(): Long = {
+    val memoryMap: collection.mutable.Map[Long, Long] = collection.mutable.Map(0L -> 0L)
+    val reg = "[0-9]+".r
+    var currentMask: String = ""
+    val data = input.map(_.split(" = "))
+    for (operation <- data){
+      if (operation(0) == "mask") currentMask = operation(1)
+      else {
+        val mem: Int = reg.findFirstIn(operation(0)).get.toInt
+        val addresses: Array[Long] = calculateAddresses(currentMask, mem.toLong)
+        for (address <- addresses) memoryMap(address) = operation(1).toLong
+      }
+
+    }
+    memoryMap.values.sum
+  }
+
+  override def getSolution(): (Long, Long) = (firstPart(), secondPart())
+
+}
